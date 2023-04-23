236cda95729e59f9d46d4c24682d29ea36d21814
Carrie Ng <114375756+carrie823@users.noreply.github.com>
2023-04-23 13:44:47 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-//import Projectinfro from "./components/Projectinfo";
+
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Education from "./routes/Education";
 import Contact from "./routes/Contact";
 
-// import image from "./images/drawing.png";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        {/* <image /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
