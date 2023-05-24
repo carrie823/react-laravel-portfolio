cfd9050cb9cc91b7aec4e14da66e513c05a153d5
Carrie Ng <114375756+carrie823@users.noreply.github.com>
2023-05-24 10:38:00 -0400

    Update About.jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -5,7 +5,8 @@ export default function About() {
     <main id="about-main">
       <h1>About</h1>
       <h2>Hi, I'm Carrie!</h2>
-      <p>I am currently a Humber College student doing a Post-Graduate Certificate in Web Development. I enjoying doing front-end web developement and digital illustration. I am knowledgeable in JavaScript, HTML/CSS, React, SQL, C#, ASP.NET, and PHP.</p>
+      <p>I am currently a Humber College student doing a Post-Graduate Certificate in Web Development. I enjoying doing front-end web developement and digital illustration. I enjoy merging creativity and technology to build modern websites, and bringing my ideas to life on a digital canvas. I strive to make websites which are responsive, accessible, and functional. I'm always excited to take on a programming challenge.
+I am knowledgeable in JavaScript, HTML/CSS, React, SQL, C#, ASP.NET, and PHP.</p>
 
       <div class="pic">
         <img src= {image} alt="digital illustration of girl" width="300"/>
