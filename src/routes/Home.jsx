5ec0b425c0c1a7f6dd12bf1906556dde9c5cf10e
Carrie Ng <114375756+carrie823@users.noreply.github.com>
2023-07-14 23:30:00 -0400

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,11 +4,14 @@ export default function Home() {
   return(
     <main id="main">
       <h2 id="subsite-name">
-        Web Developer & Digital Illustrator
+        Web Developer & Illustrator
       </h2>
       <div id="home-about">
         <h2 id="main-heading">Hi there! I'm Carrie!</h2>
-        <p>Welcome to my portfolio! Take a look around, I might be the web dev you've been looking for to make your website a reality!</p>
+        <p>Welcome to my portfolio!</p>
+        <div>
+          <p>Take a look around, I might be the web dev you've been looking for to make your website a reality!</p>
+        </div>
       </div>
       <div class="pic">
         <img src= {image} alt="digital illustration of girl" width="300"/>
